7b55b41a8a58138235579b58589ea06bfc8e9692
NOT671 <2762193314@qq.com>
2024-02-22 20:26:07 +0800

    绘图测试

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include "terminal.h"
+
+using namespace std::chrono_literals;
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    tc::hide_corsor();
+
+    int i = 1;
+
+    while(true)
+    {
+        tc::clean_cereen();
+        tc::move_to(i++, 10);
+        tc::set_back_color(15);
+        std::cout << "  ";
+        tc::reset_color();
+        std::cout<<std::flush;
+        std::this_thread::sleep_for(500ms);
+    }
 
     return 0;
 }
